2e4f2ec5b37507abd94696b4c5aeaeb4b5f3c89d
DariaKorchakovska <gospodiskoro20@gmail.com>
2024-05-25 10:47:22 +0300

    update base

--- expenses_monitoring/views.py
+++ expenses_monitoring/views.py
@@ -226,14 +226,14 @@ def filter_expenses(request):
 def expense_analysis(request):
     start_of_current_month, current_time = get_latest_bounds()
     user = request.user
-    if Expense.objects.filter(user=user).count() == 0:
-        load_expenses_from_files(user)
+    # if Expense.objects.filter(user=user).count() == 0:
+    #     load_expenses_from_files(user)
     # else:
-    #     thread = threading.Thread(
-    #         target=fetch_and_update_expenses,
-    #         args=(user, start_of_current_month, current_time),
-    #     )
-    #     thread.start()
+    thread = threading.Thread(
+        target=fetch_and_update_expenses,
+        args=(user, start_of_current_month, current_time),
+    )
+    thread.start()
     return render(request, "expense_analysis.html")
 
 
